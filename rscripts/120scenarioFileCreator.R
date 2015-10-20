e1c1f2b4b8c88d30fefddaf37944971b8e86999b
rjjdejeu <rjjdejeu@gmail.com>
2015-10-20 10:15:56 +0200

    update

--- rscripts/120scenarioFileCreator.R
+++ rscripts/120scenarioFileCreator.R
@@ -2,7 +2,7 @@
 
 # Step 1 building the scenarios: insert dataframe and read the scenario file. Name parameters
 # that need to be replaced with #
-xmlFilePath<-"~/Desktop/emlabGen/scenarios/RDJ-RT-TC-ThreeMultiNodes.xml"
+xmlFilePath<-"~/Desktop/emlabGen/scenarios/RDJ-RT-TC-ThreeMultiNodes-Template.xml"
 filestump<-'RDJ-RT-TC-ThreeMultiNodes-'
 # Step 2 building the scenarios: make separate data vectors
 noOfRepetitions = 120 
@@ -10,10 +10,10 @@ for(runID in seq(1:noOfRepetitions))
   {
   xmlFileContent<-readLines(xmlFilePath, encoding = "UTF-8")
   xmlFileContent<-gsub("#repetitionNumber", runID, xmlFileContent)
-  writeLines(xmlFileContent, paste("~/Desktop/emlabGen/scenarios/RDJ-RT-TC-ThreeMultiNodes/", filestump, runID, ".xml", sep=""))
+  writeLines(xmlFileContent, paste("~/Desktop/emlabGen/scenarios/RDJ-RT-TC-ThreeMultiNodes-Template/", filestump, runID, ".xml", sep=""))
   }
 
-xmlFilePath<-"~/Desktop/emlabGen/scenarios/RDJ-RT-TC-OneMultiNodes.xml"
+xmlFilePath<-"~/Desktop/emlabGen/scenarios/RDJ-RT-TC-OneMultiNodes-Template.xml"
 filestump<-'RDJ-RT-TC-OneMultiNodes-'
 # Step 2 building the scenarios: make separate data vectors
 noOfRepetitions = 120 
@@ -21,5 +21,5 @@ for(runID in seq(1:noOfRepetitions))
 {
   xmlFileContent<-readLines(xmlFilePath, encoding = "UTF-8")
   xmlFileContent<-gsub("#repetitionNumber", runID, xmlFileContent)
-  writeLines(xmlFileContent, paste("~/Desktop/emlabGen/scenarios/RDJ-RT-TC-OneMultiNodes/", filestump, runID, ".xml", sep=""))
+  writeLines(xmlFileContent, paste("~/Desktop/emlabGen/scenarios/RDJ-RT-TC-OneMultiNodes-Template/", filestump, runID, ".xml", sep=""))
 }
